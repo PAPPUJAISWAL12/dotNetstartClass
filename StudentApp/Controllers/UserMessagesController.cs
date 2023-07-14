b5c6581091d990e72c85f9fad5f286e5726bbb92
PAPPUJAISWAL12 <jaiswalpappu186@gmail.com>
2023-07-14 12:43:23 +0545

    ajax implementation with contact

---
+++ StudentApp/Controllers/UserMessagesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentApp.Models;
+
+namespace StudentApp.Controllers
+{
+	public class UserMessagesController : Controller
+	{
+		private readonly StudentBlogContext _context;
+
+		public UserMessagesController(StudentBlogContext context)
+		{
+			_context = context;
+		}	
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return PartialView("_Create");
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create([Bind("MsgId,Uname,Uaddress,Uphone,Umsg")] UserMessage msg)
+		{
+			if (msg != null)
+			{
+				_context.Add(msg);
+				await _context.SaveChangesAsync();
+				/*return RedirectToAction("Index", "Students");*/
+				return Content("success");
+			}
+			else
+			{
+				return Content("failled");
+			}
+			
+			
+		}
+	}
+	
+}
